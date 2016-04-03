5d72dcf1ac0120415077ab26e9e4caeae957ecfa
Andrew Lobban <andrewglobban@gmail.com>
2016-04-03 06:47:44 -0500

    Add MongoDB Support

--- apsi.js
+++ apsi.js
@@ -1,5 +1,12 @@
 var express = require("express");
+var bodyParser = require("body-parser");
+var cors = require("cors");
+var MongoClient = require("mongodb").MongoClient;
 var app = express();
+
+MongoClient.connect("mongodb://localhost:27017/zeta", function(err, db) {
+
+});
 
 app.use(express.static(__dirname + "/public"));
 
