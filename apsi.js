243da0b8dcf74344d97d7c6046453b8694cb94fc
Andrew Lobban <andrewglobban@gmail.com>
2016-08-27 22:33:29 -0500

    Extract Executive routes

--- apsi.js
+++ apsi.js
@@ -1,16 +1,31 @@
 var express = require("express");
 var bodyParser = require("body-parser");
 var cors = require("cors");
-var MongoClient = require("mongodb").MongoClient;
-var app = express();
+// var MongoClient = require("mongodb").MongoClient;
+// var db = MongoClient.connect("mongodb://localhost:27017/zeta");
+var mongoose = require("mongoose");
 
-MongoClient.connect("mongodb://localhost:27017/zeta");
+var db = mongoose.connect('mongodb://localhost/zeta');
+
+var Executive = require('./models/executiveModel');
+
+var app = express();
+var port = process.env.PORT || 3000;
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
+executiveRouter = require('./routes/executiveRoutes')(Executive);
+
+app.use('/api', executiveRouter);
+
 
 app.use(express.static(__dirname + "/public"));
 
-app.listen(3000);
+app.listen(port, function () {
+    console.log("Express app running on port "+port);
+    console.log("http://localhost:"+port);
+});
 
-console.log("Express app running on port 3000");
-console.log("http://localhost:3000");
 
 module.exports = app;
---
+++ routes/executiveRoutes.js
@@ -0,0 +1,45 @@
+/**
+ * Created by andrewlobban on 8/27/16.
+ */
+var express = require('express');
+
+var routes = function(Executive) {
+
+    var executiveRouter = express.Router();
+
+    executiveRouter.route('/executives')
+        .post(function(req, res) {
+            var exec = new Executive(req.body);
+
+            exec.save();
+
+            res.status(201);
+            res.send(exec);
+        })
+        .get(function(req, res) {
+            // var responseJson = { hello: 'this is my api!' };
+            Executive.find(function(err, executives) {
+                if(err)
+                    console.log(err);
+                else
+                    res.json(executives);
+            });
+            // res.json(responseJson);
+        });
+
+    executiveRouter.route('/executives/:executiveId')
+        .get(function(req, res) {
+            // var responseJson = { hello: 'this is my api!' };
+            Executive.findById(req.params.executiveId, function(err, executive) {
+                if(err)
+                    console.log(err);
+                else
+                    res.json(executive);
+            });
+            // res.json(responseJson);
+        });
+
+    return executiveRouter;
+};
+
+module.exports = routes;
