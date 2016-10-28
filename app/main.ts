e46f73cc25e2e20e6889fd5797486997dbddc96a
Andrew Lobban <andrewglobban@gmail.com>
2016-10-28 00:38:15 -0500

    Install and configure Angular 2

---
+++ app/main.ts
@@ -0,0 +1,7 @@
+/**
+ * Created by andrewlobban on 10/28/16.
+ */
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { AppModule } from './app.module';
+const platform = platformBrowserDynamic();
+platform.bootstrapModule(AppModule);
